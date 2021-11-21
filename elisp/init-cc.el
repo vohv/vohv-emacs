d4807eff733b4d8069d0462448e30eaf2456a88f
Zhuohui Zou <zhuohui-third@outlook.com>
2021-11-21 22:44:02 +0800

    u
    Signed-off-by: Zhuohui Zou <zhuohui-third@outlook.com>

--- elisp/init-cc.el
+++ elisp/init-cc.el
@@ -1,6 +1,41 @@
 ;; -*- lexical-binding: t -*-
 
+(straight-use-package 'flycheck)
+
+(with-eval-after-load "flycheck"
+  (add-to-list 'flycheck-checkers 'c/c++-clang-check))
+
+(defvar flycheck-clangcheck-build-path "build")
+
+(defun flycheck-clangcheck-find-project-root (checker)
+  (projectile-project-root))
+(flycheck-define-checker c/c++-clang-check
+  "A C/C++ syntax checker using Clang.
+
+See URL `http://clang.llvm.org/'."
+  :command ("clang-check"
+            (option "-p" flycheck-clangcheck-build-path)
+            "--extra-arg=-Wno-unknown-warning-option"  ; silence GCC options
+            "--extra-arg=-Wno-null-character"          ; silence null
+            "--extra-arg=-fno-color-diagnostics"       ; Do not include color codes in output
+            "--extra-arg=-fno-caret-diagnostics"       ; Do not visually indicate the source
+            "--extra-arg=-fno-diagnostics-show-option" ; Do not show the corresponding
+            source-inplace
+            ;; source-original
+            )
+  :error-patterns
+  ((info line-start (file-name) ":" line ":" column
+         ": note: " (optional (message)) line-end)
+   (warning line-start (file-name) ":" line ":" column
+            ": warning: " (optional (message)) line-end)
+   (error line-start (file-name) ":" line ":" column
+          ": " (or "fatal error" "error") ": " (optional (message)) line-end))
+  :modes (c-mode c++-mode)
+  :predicate (lambda () (flycheck-buffer-saved-p))
+  :working-directory flycheck-clangcheck-find-project-root)
+
 (add-hook 'c-mode-common-hook (lambda ()
+                                (flycheck-mode 1)
                                 (define-key c-mode-base-map (kbd "C-x C-o") #'ff-find-other-file)
                                 (c-set-style "stroustrup")
                                 (setq-default c-basic-offset 4)))
