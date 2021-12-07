f89e03273d5a36ff6e1cf9fb491f6ce058ba5b84
Zhuohui Zou <zhuohui@xsky.com>
2021-12-07 15:37:09 +0800

    u
    Signed-off-by: Zhuohui Zou <zhuohui@xsky.com>

--- elisp/init-const.el
+++ elisp/init-const.el
@@ -1,7 +1,9 @@
 ;;; -*- lexical-binding: t -*-
 
 (defconst sys/wslp
-  (string-match-p "microsoft" operating-system-release)
+  (when operating-system-release
+    (string-match-p "microsoft" operating-system-release))
+  
   "Are we running on Microsoft WSL?")
 
 (defconst sys/win32p
